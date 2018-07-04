25ce642547a0cc17a23d6481f836e562b17d6678
myhackinfo <35182611+MyHackInfo@users.noreply.github.com>
2018-07-04 12:04:49 +0530

    Delete 05-Clear_Screen..py

--- 05-Clear_Screen..py
+++
@@ -1,6 +0,0 @@
-# any libraries import by using import keyword
-# in this code we import os libraries 
-# Clear Screen by type "clear" word we create
-print('this is testing of clear')
-import os 
-clear=lambda: os.system('cls')
